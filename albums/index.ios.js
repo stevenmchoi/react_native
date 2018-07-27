37a38f2156be33f16fd58f028e7a00b648b9f01e
Steven Choi <steven.m.choi@gmail.com>
2018-07-27 03:33:03 -0700

    Register App component

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,20 +1,19 @@
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 
 // Create a component
-const App = () => {
-    return (
-        <Text>Some Text</Text>
-    );
-};
+const App = () => (
+    <Text>Some Text</Text>
+);
 
 // Render it to the device
+AppRegistry.registerComponent(appName, () => App);
 
 // /** @format */
 
 // import { AppRegistry } from 'react-native';
 // import App from './App';
-// import { name as appName } from './app.json';
 
 // AppRegistry.registerComponent(appName, () => App);
